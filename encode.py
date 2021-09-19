c3c4c64163eeffec1d443e6eac1bc709350666c7
macx78 <mark.camp715@gmail.com>
2021-09-19 09:05:15 -0400

    add serialize json into python add load json into python objects add print - login object
    Signed-off-by: macx78 <mark.camp715@gmail.com>

--- encode.py
+++ encode.py
@@ -1,10 +1,15 @@
-
+import json
 
 with open('C:/Users/markc_gphih48/Documents/development/python/json-encoder/data/creds.json','r') as reader:
-    for line in reader:
-        if line.find('username') >= 0:
-            print(line)
-            print(len(line))
+    # for line in reader:
+    #     if line.find('username') >= 0:
+    #         print(line)
+    #         print(len(line))
+    datas = json.load(reader)
 
+# with open('C:/Users/markc_gphih48/Documents/development/python/json-encoder/data/credsw.json','w') as json_file:
+#     json.dump(data, json_file, indent=4)
 
-
+for data in datas['items']:
+    if data['login']:
+        print(data['login'])
