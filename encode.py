65224b3495b7e647873c88f2043f3e49635d272f
macx78 <mark.camp715@gmail.com>
2021-09-21 19:57:33 -0400

    read from json bitwarden export process to json for input into db
    Signed-off-by: macx78 <mark.camp715@gmail.com>

--- encode.py
+++ encode.py
@@ -6,10 +6,26 @@ with open('C:/Users/markc_gphih48/Documents/development/python/json-encoder/data
     #         print(line)
     #         print(len(line))
     datas = json.load(reader)
-
-# with open('C:/Users/markc_gphih48/Documents/development/python/json-encoder/data/credsw.json','w') as json_file:
-#     json.dump(data, json_file, indent=4)
-
+count = 1
+creds = []
 for data in datas['items']:
+    logins = {}
+    print(data)
     if data['login']:
-        print(data['login'])
+        print(count)
+        cred = data['login']
+        username = cred['username']
+        password = cred['password']
+        logins['username'] = username
+        logins['password'] = password
+        if 'uris' in cred.keys():
+            d = cred['uris'].pop()
+            url = d['uri']
+            logins['url'] = url
+            creds.append(logins)
+
+with open('C:/Users/markc_gphih48/Documents/development/python/json-encoder/data/credsw.json','w') as json_file:
+    json.dump(creds, json_file, indent=4)
+
+
+
